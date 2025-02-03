2b1e2c63e23abec5443a70238171454dba441b59
MickeyG-Dev <38441045+MickeyG-Dev@users.noreply.github.com>
2025-02-03 17:58:07 +0000

    SQL File Added

---
+++ SQL.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `kuban_comp` (
+	`code` TEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+	`item` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+	`amount` INT(11) NULL DEFAULT NULL
+)
+COLLATE='utf8mb3_general_ci'
+ENGINE=InnoDB
+;
