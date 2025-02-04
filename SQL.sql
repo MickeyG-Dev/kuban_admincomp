b0521485b0d6ee6110d5e4bbeb14f2da8b516234
MickeyG-Dev <38441045+MickeyG-Dev@users.noreply.github.com>
2025-02-04 15:34:04 +0000

    Delete SQL.sql

--- SQL.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `kuban_comp` (
-	`code` TEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`item` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`amount` INT(11) NULL DEFAULT NULL
-)
-COLLATE='utf8mb3_general_ci'
-ENGINE=InnoDB
-;
